dcff1c03bc82299789f8b2d77ebb65be09863f04
Abhijeet1710 <abhijeetkhamkar30.com>
2021-07-14 11:21:09 +0530

    corousal added

--- src/layouts/Default.layout.jsx
+++ src/layouts/Default.layout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import HeroCorousal from "../components/HeroCorousal/HeroCorousal.components";
 import Navbar from "../components/navbar/navbar.components";
 
 const DefaultLayout = (props) => {
   return (
     <>
       <Navbar />
+      <HeroCorousal />
       {props.children}
     </>
   );
